5e4bc083c39b9e4af3f7c249eef43211cb9d2eca
351157 <351157@0338S-S-R0B6H0G.sdcs.local>
2018-11-27 14:24:16 -0800

    birthday reminder, robot color choose ip

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -14,20 +14,36 @@ public class BirthdayReminder {
 		String momsBirthday = "March 1st";
 		String dadsBirthday = "Jan 1st";
 		String myBirthday = "August 6th";
+		String kevinsBirthday = "November 4th";
+		String trasBirthday = "Jan 17th";
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	String bbb = JOptionPane.showInputDialog("Whose birthday would you like to know?");
+	String bbb = JOptionPane.showInputDialog("Whose birthday would you like to know?\n Mom\nDad\nReyna\nKevin\nTra");
 		// 3. Print out what the user typed
 		System.out.println(bbb);
 		// 4. if user asked for "mom"
 		if (bbb.equalsIgnoreCase("mom")) {
-			System.out.println(momsBirthday);
-		}
 			//print mom's birthday
+		System.out.println(momsBirthday);
+	}
 		// 5. if user asked for "dad"
+			else if(bbb.equalsIgnoreCase("dad")) {
 			// print dad's birthday
+				System.out.println(dadsBirthday);
+			}
 		// 6. if user asked for your name
-			// print myBirthday
+			else if(bbb.equalsIgnoreCase("reyna")) {
+				// print myBirthday	
+				System.out.println(myBirthday);
+			}
+			else if(bbb.equalsIgnoreCase("kevin")) {
+				System.out.println(kevinsBirthday);
+			}
+			else if(bbb.equalsIgnoreCase("Tra")) {
+				System.out.println(trasBirthday);
+			}sssss
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+			else {
+				System.out.println("Sorry, i don't remember that person's birthday!");
+			}
 	} 
 }
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,15 +4,48 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot spencer = new Robot() ;
 		//3. Ask the user what color they would like the robot to draw
-		
+		String color = JOptionPane.showInputDialog("What color would you like the robot to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
+if (color.equalsIgnoreCase("pink")) {
+	spencer.setPenColor(Color.pink);
+}
+else if(color.equalsIgnoreCase("blue")) {
+	spencer.setPenColor(Color.blue);
+}
+else if(color.equalsIgnoreCase("orange")) {
+	spencer.setPenColor(Color.orange);
+}
+else if(color.equalsIgnoreCase("magenta")) {
+	spencer.setPenColor(Color.magenta);
+}
+else if(color.equalsIgnoreCase("cyan")) {
+	spencer.setPenColor(Color.cyan);
+}
+else if(color.equalsIgnoreCase("gray")) {
+	spencer.setPenColor(Color.gray);
+}
+else if(color.equalsIgnoreCase("red")) {
+	spencer.setPenColor(Color.red);
+}
+else if(color.equalsIgnoreCase("black")) {
+	spencer.setPenColor(Color.black);
+}
+else if(color.equalsIgnoreCase("green")) {
+	spencer.setPenColor(Color.green);
+}
+else if(color)
+
+
 
         //6. If the user doesn’t enter anything, choose a random color
 
