94bca9bc34ff74d0599f7e2044afc1ab46f79a2b
351157 <351157@0338S-S-R0B6H0G.sdcs.local>
2019-01-15 14:23:07 -0800

    years alive, Walk of fame ip

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,17 +9,25 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
+		rob.setX(70);
 		// You also need to show the robot to see the result of this line.
 
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		for (int i = 0; i < 5; i++) {
+			
+		rob.setSpeed(50);
+		rob.penDown();
+		rob.turn(144);
+		rob.move(30);
+		rob.hide();
+		}
+		
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walkOfFame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walkOfFame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
---
+++ src/loops/yearsAlive.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class yearsAlive {
+	public static void main(String[] args) {
+
+		int i = (2004);
+		for (i = 2004; i < 2020; i++) {
+
+			System.out.println(i);
+
+		}
+	}
+}
